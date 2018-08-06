bd14643faafff8380966f9185bcc91b01a14c7cc
Hector <hector.acosta.pozo@outlook.com>
2018-08-06 11:50:20 -0700

    Establish comunication

--- Program.cs
+++ Program.cs
@@ -1,14 +1,212 @@
 ﻿using System;
 using System.Reflection;
-using System.Collections.Generic;
 using Microsoft.Extensions.CommandLineUtils;
 using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
 
 namespace pmessenger
 {
     class Program
     {
+        static Spinner spinner;
+        static int PORT = 10094;
+        static string SERVER_IP = "127.0.0.1";
+        static Socket serverSocket;
+
+        private static void StartServer()
+        {
+            spinner.Start();
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            serverSocket.Bind(new IPEndPoint(IPAddress.Any, PORT));
+            serverSocket.Listen(100);
+            serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+            spinner.Stop();
+
+            Console.WriteLine("\nServer ip: {0}", SERVER_IP);
+            Console.WriteLine("Server port: {0}", PORT);
+            Console.WriteLine("SERVER STARTED | Displaying log...");
+
+            string result = "";
+            do
+            {
+                result = Console.ReadLine();
+            } while (result.ToLower().Trim() != "exit");
+
+        }
+
+        private const int BUFFER_SIZE = 8192;
+        private static byte[] buffer = new byte[BUFFER_SIZE];
+        private static void AcceptCallback(IAsyncResult result)
+        {
+            Socket socket = null;
+            try
+            {
+                socket = serverSocket.EndAccept(result);
+                socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+                serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+            }
+            catch (Exception e) {           
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        const int MAX_RECEIVE_ATTEMPT = 10;
+        static int receiveAttempt = 0;
+        private static void ReceiveCallback(IAsyncResult result)
+        {
+            Socket socket = null;
+            try
+            {
+                socket = (Socket)result.AsyncState;
+                if (socket.Connected)
+                {
+                    int received = socket.EndReceive(result);
+                    if (received > 0)
+                    {
+                        byte[] data = new byte[received];
+                        Buffer.BlockCopy(buffer, 0, data, 0, data.Length);
+                        Console.WriteLine(Encoding.UTF8.GetString(data)); //Here I just print it, but you need to do something else                     
+
+                        //Message retrieval part
+                        //Suppose you only want to declare that you receive data from a client to that client
+                        string msg = "I receive your BALALAL message on: " + DateTime.Now;
+                        socket.Send(Encoding.ASCII.GetBytes(msg)); //Note that you actually send data in byte[]
+                        Console.WriteLine("I sent this message to the client: " + msg);
+
+                        receiveAttempt = 0; //reset receive attempt
+                        socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket); //repeat beginReceive
+                    }
+                    else if (receiveAttempt < MAX_RECEIVE_ATTEMPT)
+                    { //fail but not exceeding max attempt, repeats
+                        ++receiveAttempt; //increase receive attempt;
+                        socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket); //repeat beginReceive
+                    }
+                    else
+                    { //completely fails!
+                        Console.WriteLine("receiveCallback fails!"); //don't repeat beginReceive
+                        receiveAttempt = 0; //reset this for the next connection
+                    }
+                }
+            }
+            catch (Exception e)
+            { // this exception will happen when "this" is be disposed...
+                Console.WriteLine("receiveCallback fails with exception! " + e.ToString());
+            }
+        }
+
+        static Socket clientSocket;
+
+        private static void RegisterClient()
+        {
+            clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            LoopConnect(3, 3);
+            string result = "";
+            do
+            {
+                result = Console.ReadLine(); //you need to change this part
+                if (result.ToLower().Trim() != "exit")
+                {
+                    byte[] bytes = Encoding.ASCII.GetBytes(result); //Again, note that your data is actually of byte[], not string
+                    //do something on bytes by using the reference such that you can type in HEX STRING but sending thing in bytes
+                    clientSocket.Send(bytes);
+                }
+            } while (result.ToLower().Trim() != "exit");
+        }
+
+        static void LoopConnect(int noOfRetry, int attemptPeriodInSeconds)
+        {
+            int attempts = 0;
+            while (!clientSocket.Connected && attempts < noOfRetry)
+            {
+                try
+                {
+                    ++attempts;
+                    IAsyncResult result = clientSocket.BeginConnect(IPAddress.Parse(SERVER_IP), PORT, EndConnectCallback, null);
+                    result.AsyncWaitHandle.WaitOne(TimeSpan.FromSeconds(attemptPeriodInSeconds));
+                    System.Threading.Thread.Sleep(attemptPeriodInSeconds * 1000);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error: " + e.ToString());
+                }
+            }
+            if (!clientSocket.Connected)
+            {
+                Console.WriteLine("Connection attempt is unsuccessful!");
+                return;
+            }
+        }
+
+        private static void EndConnectCallback(IAsyncResult ar)
+        {
+            try
+            {
+                clientSocket.EndConnect(ar);
+                if (clientSocket.Connected)
+                {
+                    clientSocket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallbackClient), clientSocket);
+                }
+                else
+                {
+                    Console.WriteLine("End of connection attempt, fail to connect...");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("End-connection attempt is unsuccessful! " + e.ToString());
+            }
+        }
+
+        static int receiveAttemptClient = 0;
+
+        private static void ReceiveCallbackClient(IAsyncResult result)
+        {
+            System.Net.Sockets.Socket socket = null;
+            try
+            {
+                socket = (System.Net.Sockets.Socket)result.AsyncState;
+                if (socket.Connected)
+                {
+                    int received = socket.EndReceive(result);
+                    if (received > 0)
+                    {
+                        receiveAttemptClient = 0;
+                        byte[] data = new byte[received];
+                        Buffer.BlockCopy(buffer, 0, data, 0, data.Length); //There are several way to do this according to https://stackoverflow.com/questions/5099604/any-faster-way-of-copying-arrays-in-c in general, System.Buffer.memcpyimpl is the fastest
+                        //DO ANYTHING THAT YOU WANT WITH data, IT IS THE RECEIVED PACKET!
+                        //Notice that your data is not string! It is actually byte[]
+                        //For now I will just print it out
+                        Console.WriteLine("Server: " + Encoding.UTF8.GetString(data));
+                        socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallbackClient), socket);
+                    }
+                    else if (receiveAttemptClient < MAX_RECEIVE_ATTEMPT)
+                    { //not exceeding the max attempt, try again
+                        ++receiveAttemptClient;
+                        socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallbackClient), socket);
+                    }
+                    else
+                    { //completely fails!
+                        Console.WriteLine("receiveCallback is failed!");
+                        receiveAttemptClient = 0;
+                        clientSocket.Close();
+                    }
+                }
+            }
+            catch (Exception e)
+            { // this exception will happen when "this" is be disposed...
+                Console.WriteLine("receiveCallback is failed! " + e.ToString());
+            }
+        }
+        
         static void Main(string[] args)
+        {
+            spinner = new Spinner(10, 10);
+            terminalControls(args);
+        }
+
+        static void terminalControls(String[] args)
         {
             var app = new CommandLineApplication();
             app.Name = "PMessenger";
@@ -28,6 +226,12 @@ namespace pmessenger
             // OPTIONS
             var startServer = app.Option("-s|--server",
                 "Initiates or stop the server", CommandOptionType.NoValue);
+            var registerClient = app.Option("-c|--client",
+                "Registers the client application", CommandOptionType.NoValue);
+            var port = app.Option("-p|--port",
+                "Set server listening port", CommandOptionType.SingleValue);
+            var ip = app.Option("-i|--ip",
+                "Set server listening ip address", CommandOptionType.SingleValue);
 
             // ADD ARGUMENT
             //var argOne = app.Argument("argOne", "App argument one");
@@ -37,20 +241,28 @@ namespace pmessenger
             app.OnExecute(() =>
             {
 
+                if (port.HasValue())
+                {
+                    PORT = Convert.ToInt32(port.Value());
+                }
+
+                if (ip.HasValue())
+                {
+                    SERVER_IP = ip.Value();
+                }
+
                 if (startServer.HasValue())
                 {
-                    Console.WriteLine("Starting server");
-                    var spinner = new Spinner(10, 10);
-
-                    spinner.Start();
-                    
-                    Thread.Sleep(10000);
-
-                    spinner.Stop();
+                    Console.WriteLine("Starting server...");
+                    StartServer();
                 }
-                else {
-                    Console.WriteLine("Dont");
+
+                if (registerClient.HasValue())
+                {
+                    Console.WriteLine("Registering client");
+                    RegisterClient();
                 }
+
 
                 return 0;
             });
@@ -147,7 +359,6 @@ namespace pmessenger
 
             try
             {
-                Console.WriteLine("PMessenger executing...");
                 app.Execute(args);
             }
             catch (CommandParsingException ex)
@@ -158,7 +369,6 @@ namespace pmessenger
             {
                 Console.WriteLine("Unable to execute application: {0}", ex.Message);
             }
-
         }
     }
 
