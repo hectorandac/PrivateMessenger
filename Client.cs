6b0d799cc811d3e4773b7c6467ed97d239e5c212
Hector <hector.acosta.pozo@outlook.com>
2018-08-09 19:29:47 -0700

    Adding missing files

---
+++ Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace pmessenger
+{
+    class Client
+    {
+        public Socket ClientSocket { get; set; }
+        public string SessionId { get; set; }
+        public string PublicKey { get; set; }
+
+        public Client(Socket s, string id, string pk)
+        {
+            this.ClientSocket = s;
+            this.SessionId = id;
+            this.PublicKey = pk;
+        }
+    }
+}
